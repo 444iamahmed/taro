d8d74af03a38f6f1c2092d71dde01b5cc20fb195
444iamahmed <444iamahmed@gmail.com>
2021-11-13 20:28:48 +0500

    add parsing capability to VectorSprite

--- src/components/VectorSprite.js
+++ src/components/VectorSprite.js
@@ -11,7 +11,11 @@ class VectorSprite {
         this.ref = typeof data.asset === 'object' ? data.asset : this.app.assets.get(data.asset);
 
         console.log(data.asset);
-        this.loadSVG(data.asset, this);
+
+        if (data.parse)
+            this.parseSVG(data.asset);
+        else
+            this.loadSVG(data.asset, this);
 
 
         this.addEventListener('enable', this.onEnable);
@@ -35,6 +39,85 @@ class VectorSprite {
 
     }
 
+    parseSVG(svg) {
+        let data = svgLoader.parse(svg);
+
+        const paths = data.paths;
+
+        const group = new THREE.Group();
+        group.scale.multiplyScalar(0.25);
+        group.position.x = - 70;
+        group.position.y = 70;
+        group.scale.y *= - 1;
+
+
+
+        for (let i = 0; i < paths.length; i++) {
+
+            const path = paths[i];
+
+            const fillColor = path.userData.style.fill;
+            if (fillColor !== undefined && fillColor !== 'none') {
+
+                const material = new THREE.MeshBasicMaterial({
+                    color: new THREE.Color().setStyle(fillColor),
+                    opacity: path.userData.style.fillOpacity,
+                    transparent: path.userData.style.fillOpacity < 1,
+                    side: THREE.DoubleSide,
+                    depthWrite: false
+                });
+
+                const shapes = SVGLoader.createShapes(path);
+
+                for (let j = 0; j < shapes.length; j++) {
+
+                    const shape = shapes[j];
+
+                    const geometry = new THREE.ShapeGeometry(shape);
+                    const mesh = new THREE.Mesh(geometry, material);
+
+                    group.add(mesh);
+
+                }
+
+            }
+
+            const strokeColor = path.userData.style.stroke;
+
+            if (strokeColor !== undefined && strokeColor !== 'none') {
+
+                const material = new THREE.MeshBasicMaterial({
+                    color: new THREE.Color().setStyle(strokeColor),
+                    opacity: path.userData.style.strokeOpacity,
+                    transparent: path.userData.style.strokeOpacity < 1,
+                    side: THREE.DoubleSide,
+                    depthWrite: false
+                });
+
+                for (let j = 0, jl = path.subPaths.length; j < jl; j++) {
+
+                    const subPath = path.subPaths[j];
+
+                    const geometry = SVGLoader.pointsToStroke(subPath.getPoints(), path.userData.style);
+
+                    if (geometry) {
+
+                        const mesh = new THREE.Mesh(geometry, material);
+
+                        group.add(mesh);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+
+        instance.onLoad(url, group);
+    }
+
     loadSVG(url, instance) {
 
 
@@ -44,10 +127,10 @@ class VectorSprite {
             const paths = stuff.paths;
 
             const group = new THREE.Group();
-            group.scale.multiplyScalar( 0.25 );
-					group.position.x = - 70;
-					group.position.y = 70;
-					group.scale.y *= - 1;
+            group.scale.multiplyScalar(0.25);
+            group.position.x = - 70;
+            group.position.y = 70;
+            group.scale.y *= - 1;
 
 
 
